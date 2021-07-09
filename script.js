660d3db77adf52bcaf2507241b595c97c736903b
Manoj Singh <manojsinghrana467@gmail.com>
2021-07-09 23:29:15 +0530

    update

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
  */
 
 // API_KEY for maps api
-let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
+let API_KEY = "17a2350178ab846f2ec7639f8b07c7b7";
 
 /**
  * Retrieve weather data from openweathermap
@@ -17,9 +17,16 @@ let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
 getWeatherData = (city) => {
   const URL = "https://api.openweathermap.org/data/2.5/weather";
   //HINT: Use template literals to create a url with input and an API key
-
+    const FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=celcius`;
+    // console.log(FULL_URL);
+    let weatherPromise = fetch(FULL_URL);
+  return weatherPromise.then((response) => {
+    return response.json();
+        // console.log(response);
+    })
   //CODE GOES HERE
 }
+// console.log(getWeatherData("detroit"));
 
 /**
  * Retrieve city input and get the weather data
@@ -28,7 +35,13 @@ getWeatherData = (city) => {
 searchCity = () => {
   const city = document.getElementById('city-input').value;
   // CODE GOES HERE
-
+  getWeatherData(city)
+    .then((response) => {
+      // console.log(response);
+      showWeatherData(response);
+    }).catch((error) => {
+      console.log(error);
+  })
 }
 
 /**
@@ -37,6 +50,12 @@ searchCity = () => {
  */
 showWeatherData = (weatherData) => {
   //CODE GOES HERE
+  document.getElementById('city-name').innerText = weatherData.name;
+  document.getElementById('weather-type').innerText = weatherData.weather[0].main;
+  document.getElementById('temp').innerText = weatherData.main.temp;
+  document.getElementById('min-temp').innerText = weatherData.main.temp_min;
+  document.getElementById('max-temp').innerText = weatherData.main.temp_max;
+
   
 }
 
